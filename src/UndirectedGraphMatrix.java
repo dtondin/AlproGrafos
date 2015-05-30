50d76170bb898b8beb16a4726093161a52e91bc7
unknown <11280201@W1975465.portoalegre.pucrsnet.br>
2015-05-29 22:26:36 -0300

    inicio exercicios e algumas anotaoes

--- src/UndirectedGraphMatrix.java
+++ src/UndirectedGraphMatrix.java
@@ -1,23 +1,38 @@
+import java.awt.List;
 import java.util.ArrayList;
 
 
 public class UndirectedGraphMatrix implements UndirectedGraph {
+	
+	private boolean[][] matrix;
+	private List<String> names;
 
 	@Override
 	public void addVertice(String vertice) {
-		// TODO Auto-generated method stub
+		// TODO armazena o vertice em names, se ainda nao existir
 
 	}
 
 	@Override
+	/**
+	 * add o arco
+	 */
 	public void addEdge(String strOrig, String strDest) {
-		// TODO Auto-generated method stub
+		// TODO obter posicao do strOrig
+		// TODO obter posicao do strOrig
+		// TODO matr[pos(strOrig)][pos(strDest)] = true;
+		// TODO matr[pos(strDest)][pos(strOrig)] = true;
 
 	}
 
 	@Override
+	/**
+	 * obter o grau
+	 */
 	public int getDegree(String vertice) {
-		// TODO Auto-generated method stub
+		// TODO Obter posicao do vertice
+		// TODO contar quantas saidas tem o vertice na matrix
+		// TODO retornar o total
 		return 0;
 	}
 
---
+++ src/UndirectedGraphMatrixTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class UndirectedGraphMatrixTest {
+
+	@Test
+	public void test() {
+		UndirectedGraph g = new UndirectedGraphMatrix() {
+		
+		
+		}		
+
+	}
+
+}
